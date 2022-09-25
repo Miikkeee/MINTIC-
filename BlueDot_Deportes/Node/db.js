c7f84e0f0e57ec4686b4c4a706918101495d8a16
Miikkeee <109832948+Miikkeee@users.noreply.github.com>
2022-09-25 18:09:02 -0500

    Funcional

--- BlueDot_Deportes/Node/db.js
+++ BlueDot_Deportes/Node/db.js
@@ -1,88 +1,95 @@
-const equipoListar =()=>{
+const equipoListar = () => {
     equipos = [
         {
-            "id" : 1,
+            "id": 1,
             "Deporte": "Futbol",
-            "Nequipo" : "Chelsea"
+            "Nequipo": "Chelsea"
 
         },
         {
-            "id" : 2,
+            "id": 2,
             "Deporte": "Futbol",
-            "Nequipo" : "Liverpool"
+            "Nequipo": "Liverpool"
 
         },
         {
-            "id" : 3,
+            "id": 3,
             "Deporte": "Futbol",
-            "Nequipo" : "Manchester United"
+            "Nequipo": "Manchester United"
 
         },
-        
+
         {
-            "id" : 4,
+            "id": 4,
             "Deporte": "Basquetball",
-            "Nequipo" : "Lakers"
+            "Nequipo": "Lakers"
 
         }
 
 
     ]
-      
-    
 
-return equipos
+
+
+    return equipos
+
+}
+
+
+
+const user = () => {
+    users = [
+        {
+            "User": "Mike",
+            "Password": "123"
+        },
+
+        {
+            "User": "Mike2",
+            "Password": "1233214"
+        }
+    ]
+
+    return users
+
+}
+
+
+const evento = () => {
+    eventos = [
+        {
+            "id": 1,
+            "ideequipo1": 1,
+            "ideequipo2": 2,
+            "Fechaevento": "1/5/2022",
+            "Equipo1": "Chelsea",
+            "Equipo2": "Liverpool",
+            "MarcadorE1": 2,
+            "MarcadorE2": 3
+        },
+
+        {
+            "id": 2,
+            "ideequipo1": 3,
+            "ideequipo2": 2,
+            "Fechaevento": "3/7/2022",
+            "Equipo1": "Manchester United",
+            "Equipo2": "Liverpool",
+            "MarcadorE1": 3,
+            "MarcadorE2": 3
+        }
+    ]
+
+    return eventos
 
 }
 
 module.exports = {
     equipoListar
 }
-
-
-
-const usuario = ()=>{
-    usuarios =  [
-        {
-            "User" : "Mike",
-            "Password" : "1234"
-
-        },
-
-        {
-            "User" : "Serch",
-            "Password" : "1234"
-        }
-    ]  
-
-return usuario
-
-    
+module.exports = {
+    evento
 }
-
-const eventos = ()=>{
-    evento = [
-        {
-            "id" : 1,
-            "ideequipo1" : 1,
-            "ideequipo2" : 2,
-            "Fechaevento" : "1/5/2022",
-            "Equipo1" : "Chelsea",
-            "Equipo2" : "Liverpool",
-            "MarcadorE1" : 2,
-            "MarcadorE2" : 3
-        },
-
-        {
-            "id" : 2,
-            "ideequipo1" : 3,
-            "ideequipo2" : 2,
-            "Fechaevento" : "3/7/2022",
-            "Equipo1" : "Manchester United",
-            "Equipo2" : "Liverpool",
-            "MarcadorE1" : 3,
-            "MarcadorE2" : 3
-        }
-    ]
-
+module.exports = {
+    user
 }
--- BlueDot_Deportes/Node/index.js
+++ BlueDot_Deportes/Node/index.js
@@ -1,4 +1,4 @@
-const { response } = require('express')
+const { response, json } = require('express')
 const express = require('express')
 const app = express()
 
@@ -101,6 +101,119 @@ app.put('/api/equipos/:id', (request, response) => {
 
 })
 
+app.put('/api/eventos/:id', (request, response) => {  //inserta evento 
+    try {
+        const id = Number(request.params.id)
+        const eventoPut = request.body
+        const DB = require ('./db')
+        const evento = DB.evento().find(evento => evento.id ==id)
+        const newevento = {
+            id: evento.id,
+            ide1 : evento.ide1,
+            ide2 : evento.ide2,
+            Fechaevento : eventoPut.Fechaevento,
+            Equipo1 : eventoPut.Equipo1,
+            Equipo2 : eventoPut.Equipo2,
+            MarcadorE1 : eventoPut.MarcadorE1,
+            MarcadorE2 : eventoPut.MarcadorE2,
+
+        }
+        response.json(newevento).end()
+
+    }catch (error){
+
+        console.log("error")
+        response.status(400).end()
+    }
+
+
+})
+
+app.get('/api/eventos/:id', (request, response) => {
+
+    try {
+        const id = Number(request.params.id)
+        const DB = require('./db')
+        const evento = DB.evento().find(evento => evento.id == id)
+        if (evento) {
+            response.json(evento).end()
+        } else {
+            response.status(404).end()
+        }
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+}
+)
+
+app.delete('/api/eventos/:id', (request, response) => {  //Elimina un evento en especifico
+    try {
+        const id = Number(request.params.id)
+        const DB = require('./db')
+
+        const evento = DB.evento().filter(evento => evento.id != id)
+        if (evento) {
+            response.json(evento)
+            response.status(204).end()
+        } else {
+            response.status(404).end()
+        }
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+})
+
+
+app.post('/api/eventos', (request, response) => {
+    try {
+        const evento = request.body
+
+        const DB = require('./db')
+        //console.log(DB.evento)
+        const ids = DB.evento().map(evento => evento.id)
+        const maxId = Math.max(...ids)
+
+        const newevento = {
+            id: maxId + 1,
+            ide1 : evento.ide1,
+            ide2 : evento.ide2,
+            Fechaevento : evento.Fechaevento,
+            Equipo1 : evento.Equipo1,
+            Equipo2 : evento.Equipo2,
+            MarcadorE1 : evento.MarcadorE1,
+            MarcadorE2 : evento.MarcadorE2,
+
+        }
+
+        response.json(newevento)
+        response.end()
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+})
+
+app.post('/api/usuarios', (request, response) => {
+    console.log("ola")
+    try {
+        const usuario = request.body
+        const DB = require('./db')
+        console.log(DB.user)
+        const newUser =         {
+            User: usuario.User,
+            Password: usuario.Password
+        }
+
+        response.json(newUser)
+        response.end()
+    } catch(error){
+        console.log("error", error)
+    }
+
+}
+)
 const puerto = 3000
 
 app.listen(puerto, () => {
