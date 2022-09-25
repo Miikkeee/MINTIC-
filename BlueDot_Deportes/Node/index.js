eb33d53eb64907a149f0e0cf78ad8fc767080370
SergioM-R <64614737+SergioM-R@users.noreply.github.com>
2022-09-24 19:42:16 -0500

    Update index.js
    Se añadieron metodos para la consulta en la "base de datos"

--- BlueDot_Deportes/Node/index.js
+++ BlueDot_Deportes/Node/index.js
@@ -6,6 +6,32 @@ app.get('/', (request,response) => {
     response.end()
 })
 
+app.get('/api/equipos', (request,response) => {  // Enlista todos los equipos
+    const DB = require('./db')
+    response.send(DB.equipoListar())
+    response.end()
+})
+
+app.get('/api/equipos/:id', (request,response) => {  //Muestra un equipo en especifico
+    const id = Number(request.params.id)
+    const DB = require('./db')
+    const equipo = DB.equipoListar().find(equipo => equipo.id == id)
+
+    if(equipo){
+        response.json(equipo)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/equipos/:id', (request,response) => {
+    const id = Number(request.params.id)
+    const DB = require('./db')
+    const equipo = DB.equipoListar().filter(equipo => equipo.id != id)
+    response.status(204).end()
+})
+
+
 
 const puerto = 3000
 
