f5cd9be77cf5faf26126306dfd845795504f6ba9
Miikkeee <109832948+Miikkeee@users.noreply.github.com>
2022-09-25 14:12:29 -0500

    Update db.js

--- BlueDot_Deportes/Node/db.js
+++ BlueDot_Deportes/Node/db.js
@@ -1,4 +1,4 @@
-const equipoListar =()=>{
+const equipo =()=>{
     equipos = [
         {
             "id" : 1,
@@ -8,14 +8,81 @@ const equipoListar =()=>{
         },
         {
             "id" : 2,
+            "Deporte": "Futbol",
+            "Nequipo" : "Liverpool"
+
+        },
+        {
+            "id" : 3,
+            "Deporte": "Futbol",
+            "Nequipo" : "Manchester United"
+
+        },
+        
+        {
+            "id" : 4,
             "Deporte": "Basquetball",
             "Nequipo" : "Lakers"
 
         }
+
+
     ]
+      
+    
+
 return equipos
+
 }
 
 module.exports = {
     equipoListar
+}
+
+
+
+const usuario = ()=>{
+    usuarios =  [
+        {
+            "User" : "Mike",
+            "Password" : "1234"
+
+        },
+
+        {
+            "User" : "Serch",
+            "Password" : "1234"
+        }
+    ]  
+
+return usuario
+
+    
+}
+
+const eventos = ()=>{
+    evento = [
+        {
+            "id" : 1,
+            "ideequipo1" : 1,
+            "ideequipo2" : 2,
+            "Fechaevento" : "1/5/2022",
+            "Equipo1" : "Chelsea",
+            "Equipo2" : "Liverpool",
+            "MarcadorE1" : 2,
+            "MarcadorE2" : 3
+        },
+
+        {
+            "id" : 2,
+            "ideequipo1" : 3,
+            "ideequipo2" : 2,
+            "Fechaevento" : "3/7/2022",
+            "Equipo1" : "Manchester United",
+            "Equipo2" : "Liverpool",
+            "MarcadorE1" : 3,
+            "MarcadorE2" : 3
+        }
+    ]
+
 }
