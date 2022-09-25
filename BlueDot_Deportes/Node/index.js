3644f0389f3da6d12de60aeb91e4d8e8ed60dd04
SergioM-R <64614737+SergioM-R@users.noreply.github.com>
2022-09-25 13:48:45 -0500

    Update index.js
    Se añadieron los metodos PUT y POST para equipos

--- BlueDot_Deportes/Node/index.js
+++ BlueDot_Deportes/Node/index.js
@@ -1,44 +1,109 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
-app.get('/', (request,response) => {
-    response.send('<h1>Hola mundo</h1>')
-    response.end()
-})
+app.use(express.json())
 
-app.get('/api/equipos', (request,response) => {  // Enlista todos los equipos
-    const DB = require('./db')
-    response.send(DB.equipoListar())
-    response.end()
-})
-
-app.get('/api/equipos/:id', (request,response) => {  //Muestra un equipo en especifico
-    const id = Number(request.params.id)
-    const DB = require('./db')
-    const equipo = DB.equipoListar().find(equipo => equipo.id == id)
-   if(equipo){
-        response.json(equipo)
-    } else {
-        response.status(404).end()
+app.get('/', (request, response) => {
+    try {
+        response.send('<h1>Hola mundo</h1>')
+        response.end()
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
     }
 })
 
-app.delete('/api/equipos/:id', (request,response) => {  //Elimina un equipo en especifico
-    const id = Number(request.params.id)
-    const DB = require('./db')
-    
-    const equipo = DB.equipoListar().filter(equipo => equipo.id != id)
-    if(equipo){
-        response.json(equipo)
-        response.status(204).end()
-    } else {
-        response.status(404).end()
+app.get('/api/equipos', (request, response) => {  // Enlista todos los equipos
+    try {
+        const DB = require('./db')
+        response.send(DB.equipoListar())
+        response.end()
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
     }
+})
+
+app.get('/api/equipos/:id', (request, response) => {  //Muestra un equipo en especifico
+    try {
+        const id = Number(request.params.id)
+        const DB = require('./db')
+        const equipo = DB.equipoListar().find(equipo => equipo.id == id)
+        if (equipo) {
+            response.json(equipo)
+        } else {
+            response.status(404).end()
+        }
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+})
+
+app.delete('/api/equipos/:id', (request, response) => {  //Elimina un equipo en especifico
+    try {
+        const id = Number(request.params.id)
+        const DB = require('./db')
+
+        const equipo = DB.equipoListar().filter(equipo => equipo.id != id)
+        if (equipo) {
+            response.json(equipo)
+            response.status(204).end()
+        } else {
+            response.status(404).end()
+        }
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+})
+
+
+app.post('/api/equipos', (request, response) => {
+    try {
+        const equipo = request.body
+        const DB = require('./db')
+        const ids = DB.equipoListar().map(equipo => equipo.id)
+        const maxId = Math.max(...ids)
+
+        const newEquipo = {
+            id: maxId + 1,
+            Deporte: equipo.Deporte,
+            Nequipo: equipo.Nequipo
+        }
+
+        response.json(newEquipo)
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+})
+
+app.put('/api/equipos/:id', (request, response) => {
+    try {
+        const id = Number(request.params.id)
+        const equipoPut = request.body
+        const DB = require('./db')
+        const equipo = DB.equipoListar().find(equipo => equipo.id == id)
+
+        const newEquipo = {
+            id: equipo.id,
+            Deporte: equipoPut.Deporte,
+            Nequipo: equipoPut.Nequipo
+        }
+
+        response.json(newEquipo)
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+
 })
 
 const puerto = 3000
 
-app.listen(puerto, () =>{
+app.listen(puerto, () => {
     console.log("servidor activo, puerto: " + puerto)
-} )
+})
 
