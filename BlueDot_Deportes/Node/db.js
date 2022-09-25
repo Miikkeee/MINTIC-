e5c1019da7669d4f953d4d30445445e4bda4e93c
SergioM-R <64614737+SergioM-R@users.noreply.github.com>
2022-09-24 19:41:40 -0500

    Create db.js
    Se usa como base de datos.

---
+++ BlueDot_Deportes/Node/db.js
@@ -0,0 +1,21 @@
+const equipoListar =()=>{
+    equipos = [
+        {
+            "id" : 1,
+            "Deporte": "Futbol",
+            "Nequipo" : "Chelsea"
+
+        },
+        {
+            "id" : 2,
+            "Deporte": "Basquetball",
+            "Nequipo" : "Lakers"
+
+        }
+    ]
+return equipos
+}
+
+module.exports = {
+    equipoListar
+}
