ee39c4cf118c2a3a1f8a05df1540902ec8a16078
Miikkeee <109832948+Miikkeee@users.noreply.github.com>
2022-10-29 18:38:37 -0500

    login

---
+++ BlueDot_Deportes/Node/controllers/userController.js
@@ -0,0 +1,31 @@
+const { request, response } = require('express')
+const UserModel = require('../models/userscheme')
+//const bcryptjs = require('bcryptjs')
+const { model } = require('mongoose')
+
+const userGuardar = async (request, response) => { //Guardar un evento R
+    try {
+        const User = request.body
+    
+            try {
+                const modelUser = new UserModel(request.body)
+                console.log(modelUser)
+                modelUser.Password = await bcryptjs.hash(modelUser.Password,10)
+                modelUser.save()
+                response.send("Guardado con exito").end()
+            }catch(error){
+                console.log("error en el guardado de usuario" + error)
+            }
+            console.log("Usuario guardado")
+        
+
+    } catch (error) {
+        console.log("error")
+        response.status(400).end()
+    }
+}
+
+module.exports = {
+ 
+    userGuardar
+}
---
+++ BlueDot_Deportes/reactbluedot/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header, { Menu } from './elements/Header.js'
+import { ListaEventos } from './events/ListaEventos';
+import { Tablero } from './dashboard/Tablero';
+import { Login } from './users/login';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<>
+  <Router>
+    <Menu />
+    <Header />
+    <div className='container'>
+      <div className='row align-center'>
+        <div className='col m-5'>
+          <Routes>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path="/tablero/*" element={<Tablero />}></Route>
+            <Route path="/" element={<ListaEventos/>}></Route>
+            
+          </Routes>
+        </div>
+      </div>
+    </div>
+
+
+
+
+  </Router>
+
+</>
+);
+
