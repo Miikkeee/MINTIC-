70d204c88a85e0b5bbca3af4451196028885cb74
Miikkeee <109832948+Miikkeee@users.noreply.github.com>
2022-10-29 13:24:35 -0500

    auth0

--- BlueDot_Deportes/loginandregister/src/index.js
+++ BlueDot_Deportes/loginandregister/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {Auth0Provider} from '@auth0/auth0-react'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider domain='dev-80sws5wu6e5uc1s0.us.auth0.com' clientId='pTxpi1T1A1F1oVdHCCSeLOhdzwFsqXyT' redirectUri={window.location.origin}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
