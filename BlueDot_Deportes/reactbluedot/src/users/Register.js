b3eac56f885bd96c4241dcf9bb577631ac3a7a5f
Miikkeee <109832948+Miikkeee@users.noreply.github.com>
2022-10-29 19:12:50 -0500

    login & register

---
+++ BlueDot_Deportes/reactbluedot/src/users/Register.js
@@ -0,0 +1,33 @@
+export function Register(){
+
+    return <div class = "container-fluid">
+        <div class = "row">
+            <div class = "col-md-4 offset-4">
+                <form role="form">
+                    
+                    <div class = "form-group">
+                        <label for = "exampleInputEmail">
+                            Usuario
+                        </label>
+                        <input type="text" class="form-control" id="exampleInputEmail"></input>
+                    </div>
+
+                    <div class = "form-group">
+                        <label for="exampleInputPassword1">
+                           Contraseña
+                        </label>
+                        <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                    </div>                 
+                    <div class ="form-group my-4">
+                        <button type="button" class="btn btn-primary" onClick={function(){ console.log('hi')}}>
+                            Registrarse
+                        </button>
+                    </div>
+                    
+                </form>            
+            </div>
+        </div>
+
+    </div>
+
+}
